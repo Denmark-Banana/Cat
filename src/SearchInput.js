91c1fa3ddd7e1f23663bd756163f7bf4ff6284ba
Denmark-Banana <neat507kr@gmail.com>
2020-03-17 18:05:22 +0900

    Initial recent Search by cookie

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -6,6 +6,7 @@ class SearchInput {
   $randomButton = null;
   onSearch = null;
   onRandom = null;
+  recentSearchArr = [];
 
   constructor({ $target, onSearch, onRandom }) {
     this.$target = $target;
@@ -25,13 +26,53 @@ class SearchInput {
     $searchInput.placeholder = "Search";
     this.$searchInput = $searchInput;
     $search.appendChild($searchInput);
+
+    this.getAllCookie();
+    const $recentSearch = document.createElement("ul");
+    this.recentSearchArr.forEach(keyword => {
+      const element = document.createElement("li");
+      element.innerHTML = keyword;
+      $recentSearch.appendChild(element);
+    })
+    $search.appendChild($recentSearch);
+
+
     $target.appendChild($search);
 
     this.addEvent();
-    this.recentSearch();
+
+    const allValue = this.getAllCookie();
 
     console.log("SearchInput created.", this);
     this.render();
+  }
+
+  setCookie(name, value, exp) {
+    const date = new Date();
+    date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+    document.cookie =
+      name + "=" + value + ";expires=" + date.toUTCString() + ";path=/";
+    console.log(document.cookie);
+  }
+
+  deleteCookie = function(name) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
+  }
+  
+  getCookie(name) {
+    const value = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+    return value ? value[2] : null;
+  }
+
+  getAllCookie() {
+    const allCookie = document.cookie;
+    const keywordArr = allCookie?.split('; ');
+    for(let idx = 1; idx < keywordArr.length; idx++) {
+      if(idx > 5)
+        return;
+      const valueArr = keywordArr[idx]?.split('=');
+      this.recentSearchArr.push(valueArr[0]);
+    }
   }
 
   addEvent() {
@@ -43,7 +84,8 @@ class SearchInput {
 
     this.$searchInput.addEventListener("keyup", e => {
       if (e.keyCode === 13) {
-        localStorage.setItem("key", this.$searchInput.value);
+        //localStorage.setItem("key", this.$searchInput.value);
+        this.setCookie(this.$searchInput.value, this.$searchInput.value, 1);
         this.onSearch(e.target.value);
       }
     });
@@ -53,16 +95,5 @@ class SearchInput {
     });
   }
 
-  recentSearch() {
-    if(localStorage.getItem('key')) {
-      const $searchValue = document.createElement("span");
-      // $searchValue.innerHTML = localStorage.getItem('key');
-      // this.$target.appendChild($searchValue);
-      // TODO
-    }
-  }
-
-  render() {
-    
-  }
+  render() {}
 }
